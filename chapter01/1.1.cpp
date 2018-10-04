889145e2ad51cb464a20661007a4d5e736868a72
JsonZhangAA <1942143988@qq.com>
2018-10-04 15:41:03 +0800

    第一章

---
+++ chapter01/1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+	string firstname,lastname;
+	cin>>firstname>>lastname;
+	cout<<"\n"
+		<<"firstname:"<<firstname
+		<<"\tlastname:"<<lastname<<endl;
+	return 0;
+}
